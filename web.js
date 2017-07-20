75e10d2bc50a2638f2efc42e24385f63e003e58d
jamesthechamp <bajajshivansh1@gmail.com>
2017-07-20 10:38:05 +0530

    deploy to node server

--- web.js
+++ web.js
@@ -1,8 +1,9 @@
 var gzippo = require('gzippo');
-  var express = require('express');
-  var app = express();
- var logger = require('morgan');
+var express = require('express');
+var app = express();
+var logger = require('morgan');
 app.use(logger('dev'));
   // app.use(express.logger('dev'));
-  app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-  app.listen(process.env.PORT || 9000);
+// app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.use('/', express.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 9000);
